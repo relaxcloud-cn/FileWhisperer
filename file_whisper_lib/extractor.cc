22985e1282459601d5eef324e92d4dc28b7ff2a0
coalooball <cyan_cg@outlook.com>
2024-12-13 13:11:46 +0800

    添加 flavor / 重构提取逻辑

--- file_whisper_lib/extractor.cc
+++ file_whisper_lib/extractor.cc
@@ -1,15 +1,57 @@
 #include "extractor.hpp"
 
-std::vector<std::string> extract_urls(const std::string& text) {
-    static const re2::RE2 url_pattern("(https?://[^\\s\"<>{}]+)");
+namespace extractor
+{
 
-    std::vector<std::string> urls;
-    re2::StringPiece input(text);
-    std::string url;
+    using namespace whisper_data_type;
 
-    while (RE2::FindAndConsume(&input, url_pattern, &url)) {
-        urls.push_back(url);
+    std::vector<Node *> extract_urls(Node *node)
+    {
+        std::vector<Node *> nodes;
+        std::string text;
+        if (std::holds_alternative<File>(node->content))
+        {
+            File &file = std::get<File>(node->content);
+            spdlog::debug("Node[{}] file {}", node->id, file.mime_type);
+            text = decode_binary(file.content);
+        }
+        else if (std::holds_alternative<Data>(node->content))
+        {
+            Data &data = std::get<Data>(node->content);
+            spdlog::debug("Node[{}] data {}", node->id, data.type);
+            text = decode_binary(data.content);
+        }
+
+        auto urls = extract_urls_from_text(text);
+
+        spdlog::debug("Node[{}] Number of urls: {}", node->id, urls.size());
+
+        for (auto &item : urls)
+        {
+            Node *t_node = new whisper_data_type::Node{.content = whisper_data_type::Data{
+                                                         .type = "URL",
+                                                         .content = encode_binary(item)}};
+            t_node->prev = node;
+            nodes.push_back(t_node);
+        }
+
+        return nodes;
     }
 
-    return urls;
+    std::vector<std::string> extract_urls_from_text(const std::string &text)
+    {
+        std::vector<std::string> urls;
+        static const re2::RE2 url_pattern("(https?://[^\\s\"<>{}]+)");
+
+        re2::StringPiece input(text);
+        std::string url;
+
+        while (RE2::FindAndConsume(&input, url_pattern, &url))
+        {
+            urls.push_back(url);
+        }
+
+        return urls;
+    }
+
 }
--- tests/url_extract_test.cc
+++ tests/url_extract_test.cc
@@ -4,7 +4,7 @@
 TEST(EXTRACT_URLS, single_url)
 {
     std::string text = "访问我们的网站 https://www.example.com";
-    std::vector<std::string> result = extract_urls(text);
+    std::vector<std::string> result = extractor::extract_urls_from_text(text);
     EXPECT_EQ(result.size(), 1);
     if (result.size())
     {
