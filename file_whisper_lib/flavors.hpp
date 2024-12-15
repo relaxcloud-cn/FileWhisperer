6137b68b7beb47c3ea44151531ba4a6d3b6fc13b
coalooball <cyan_cg@outlook.com>
2024-12-15 17:45:17 +0800

    添加从Html中提取文本的功能

--- file_whisper_lib/flavors.hpp
+++ file_whisper_lib/flavors.hpp
@@ -13,6 +13,7 @@ namespace flavors
     std::map<Types, std::vector<ExtractFunctionModern>> flavor_extractors = {
         {Types::TEXT_PLAIN, {extractor::extract_urls}},
         {Types::IMAGE, {extractor::extract_qrcode}},
+        {Types::TEXT_HTML, {extractor::extract_html}},
         {Types::COMPRESSED_FILE, {extractor::extract_compressed_file}}};
 
     std::vector<Node *> extract(Node *node)
