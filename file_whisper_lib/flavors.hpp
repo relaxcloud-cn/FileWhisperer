35242061e003e2e2198fbbe653d390ba61ffc5c6
coalooball <cyan_cg@outlook.com>
2024-12-17 15:09:43 +0800

    添加提取时间记录功能

--- file_whisper_lib/flavors.hpp
+++ file_whisper_lib/flavors.hpp
@@ -1,6 +1,7 @@
 #include <map>
 #include <vector>
 #include <functional>
+#include <chrono>
 #include "types.hpp"
 #include "data_type.hpp"
 #include "extractor.hpp"
@@ -10,11 +11,24 @@ namespace flavors
     using namespace whisper_data_type;
 
     using ExtractFunctionModern = std::function<std::vector<Node *>(Node *)>;
-    std::map<Types, std::vector<ExtractFunctionModern>> flavor_extractors = {
-        {Types::TEXT_PLAIN, {extractor::extract_urls}},
-        {Types::IMAGE, {extractor::extract_qrcode, extractor::extract_ocr}},
-        {Types::TEXT_HTML, {extractor::extract_html}},
-        {Types::COMPRESSED_FILE, {extractor::extract_compressed_file}}};
+    
+    struct ExtractorInfo {
+        std::string name;
+        ExtractFunctionModern func;
+    };
+
+    const ExtractorInfo url_extractor{"url_extractor", extractor::extract_urls};
+    const ExtractorInfo qrcode_extractor{"qrcode_extractor", extractor::extract_qrcode};
+    const ExtractorInfo ocr_extractor{"ocr_extractor", extractor::extract_ocr};
+    const ExtractorInfo html_extractor{"html_extractor", extractor::extract_html};
+    const ExtractorInfo compressed_file_extractor{"compressed_file_extractor", extractor::extract_compressed_file};
+
+    std::map<Types, std::vector<ExtractorInfo>> flavor_extractors = {
+        {Types::TEXT_PLAIN, {url_extractor}},
+        {Types::IMAGE, {qrcode_extractor, ocr_extractor}},
+        {Types::TEXT_HTML, {html_extractor}},
+        {Types::COMPRESSED_FILE, {compressed_file_extractor}}
+    };
 
     std::vector<Node *> extract(Node *node)
     {
@@ -30,12 +44,18 @@ namespace flavors
         {
             for (const auto &extractor : it->second)
             {
-                auto extracted = extractor(node);
+                auto start = std::chrono::high_resolution_clock::now();
+                auto extracted = extractor.func(node);
+                auto end = std::chrono::high_resolution_clock::now();
+                
+                auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+                std::string key = "microsecond_" + extractor.name;
+                node->meta.map_number[key] = duration;
+                
                 nodes.insert(nodes.end(), extracted.begin(), extracted.end());
             }
         }
 
         return nodes;
     }
-
 }
