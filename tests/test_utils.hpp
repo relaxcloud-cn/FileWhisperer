9a4c5c4be63856f7d00cb535b06a9e1106a7f352
coalooball <cyan_cg@qq.com>
2025-01-14 15:37:47 +0800

    Change To Python

--- tests/test_utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <filesystem>
-
-namespace test_utils {
-    std::filesystem::path get_project_root();
-    std::filesystem::path get_test_file_path(const std::string& relative_path);
-}
